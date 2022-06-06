ed81d2c6ac2a09f892b58287366848c1dfde82fb
cinctrl <cinctrl@gmail.com>
2022-06-06 18:15:26 +0000

    Edit task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,22 @@
 /**
  * main - Entry point
  *
- * Return:  Always 0(success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("Size of char: %ld byte\(s\)", sizeof(char);
-	printf("Size of int: %ld byte\(s\)", sizeof(int);
-	printf("Size of long: %ld byte\(s\)", sizeof(long);
-	printf("Size of float: %ld byte\(s\)", sizeof(float);
-	printf("Size of long int: %ld byte\(s\)", sizeof(long int);
-	printf("Size of short: %ld byte\(s\)", sizeof(short);
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
 }
