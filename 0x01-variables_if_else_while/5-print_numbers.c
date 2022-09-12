e0959298120e9faff8c34e7c2c37f212f616d556
cinctrl <cinctrl@gmail.com>
2022-09-12 16:27:55 +0000

    Add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - outputs numbers from 0 to 9
+ *
+ * Return - success (0)
+ */
+int main(void)
+{
+	char  num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		printf("%c",num);
+	}
+	putchar('\n');
+}
