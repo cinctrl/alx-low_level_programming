709d724466d0f2b2bb07c79f6b36f2a42538dfe3
cinctrl <cinctrl@gmail.com>
2022-09-13 02:42:26 +0000

    Add task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main -prints combination of three digits seperatd bt commaand space
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != '8' || (a == '8' && (b != '9' || c != '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+					
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
