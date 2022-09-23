d6b6674ebf2c4e156a7d2de0ce09f23ad0c39696
cinctrl <cinctrl@gmail.com>
2022-09-23 07:55:27 +0000

    Update task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * isLower - Determines whether ascii is lowercase
- *
- * @c: Character
- *
- * Return: 1 if true, 0 if false
+ * cap_string - capitalize string
+ * @s: input string
+ * Description: capitalizes all words of a string
+ * Return: always (0)
  */
 
-int isLower(char c)
+char *cap_string(char *s)
 {
-	return (c >= 97 && c <= 122);
-}
+	int i;
 
-/**
- * isDelimiter - Determines whether ascii is a delimiter
- *
- * @c: Character
- *
- * Return: 1 if true, 0 if false
- */
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '!' || s[i] == '?' || s[i] == '('
+				|| s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
